21a48c40e9e521d747034b87439ba95e902e6c95
Sneha kimari <snehaeng2002@gmail.com>
2021-09-12 10:09:47 +0530

    video 6 done

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,32 +8,47 @@ public class CameraController : MonoBehaviour {
 
    public Vector3 offset;
 
-   public bool useOffsetValue;
+   public bool useOffsetValues;
 
    public float rotateSpeed;
 
+   public Transform pivot;
+
+    //Use this for initialization
     void Start()
     {
         if (!useOffsetValues)
         {
-            offset = target.position;
+            offset = target.position - transform.position;
         }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
    
-   void Update()
+    void Update()
     {
+        //Get the X position of the mouse and rotate the target
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        target.rotate(0, horizontal, 0);
-        float vertical  = Input.GetAxis("Mouse Y") * rotateSpeed;
-        target.rotate(Vvertical,0,0);
-        float desiredXAngle = target.eulerAngles.x;
-        float desiredYAngle = target.eulerAngles.y;
-        Quaternian rotation = Quaternian.Euler(desiredXAngle, desiredYAngle, 0);
+        target.Rotate(0, horizontal, 0);
 
-        
-       
+        //Get the Y position of the mouse and rotate the pivot
+        float vertical  = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(vertical,0,0);
+
+        float desiredXAngle = target.eulerAngles.x;
+        float desiredYAngle = pivot.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = target.position - (rotation*offset);
+
         // tranform.position = target.position - offset;
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+        }
         transform.LookAt(target);
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         //theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
 
@@ -29,18 +29,22 @@ public class PlayerController : MonoBehaviour
             theRB.velocity = new Vector3(theRB.velocity.x , jumpForce, theRB.velocity.z);
         }*/
 
-         moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical") ) + (transform.right * Input.GetAxis("Horizontal") );
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
 
-        if (controller.is grounded) ;
+        if (controller.isGrounded) 
         {
-            moveDirection.y = 0f;
+            
             if (Input.GetButtonDown("Jump"))
             {
                 moveDirection.y = jumpForce;
             }
         }
-        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale );
-        controller.Move(moveDirection *Time.deltaTime);
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime );
+        controller.Move(moveDirection * Time.deltaTime);
                     
              
 
