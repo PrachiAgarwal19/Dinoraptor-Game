a06cdbed3671da3258da630f746eda56418a01fc
Prachi Agarwal <agarwalprachi2901@gmail.com>
2021-09-13 17:18:25 +0530

    Video 11 : Till  6 minutes

--- Assets/Scripts/DestroyOverTime.cs
+++ Assets/Scripts/DestroyOverTime.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DestroyOverTime : MonoBehaviour
 {
     public float lifetime;
     // Start is called before the first frame update
