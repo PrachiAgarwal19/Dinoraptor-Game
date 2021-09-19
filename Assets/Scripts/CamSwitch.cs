cfb37cbdfcdfce562be7970772172525ab6fbb48
Manasvvi <manasvvi.aggarwal@gmail.com>
2021-09-20 00:31:15 +0530

    added camera 2 and removed mouse movement

---
+++ Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+
+    private float num;
+
+    void Start()
+    {
+        num=1;
+        // cam1.SetActive(true);
+        // cam2.SetActive(false);
+        // cam3.SetActive(false);
+        // cam4.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(num==1){
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+            num=0;
+        }
+        
+        if(Input.GetButtonDown("tab")){
+
+            if(cam1.activeSelf){
+                cam1.SetActive(false);
+                cam2.SetActive(true);
+            }
+            else if(cam2.activeSelf){
+                cam1.SetActive(true);
+                cam2.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -23,60 +23,62 @@ public class CameraController : MonoBehaviour {
     //Use this for initialization
     void Start()
     {
-        if (!useOffsetValues)
-        {
-            offset = target.position - transform.position;
-        }
+        offset = transform.position-target.position;
+        // if (!useOffsetValues)
+        // {
+        //     offset = target.position - transform.position;
+        // }
 
-        pivot.transform.position = target.transform.position;
-        //pivot.transform.parent = target.transform;
-        pivot.transform.parent = null;
-        Cursor.lockState = CursorLockMode.Locked;
+        // pivot.transform.position = target.transform.position;
+        // //pivot.transform.parent = target.transform;
+        // pivot.transform.parent = null;
+        // Cursor.lockState = CursorLockMode.Locked;
     }//update is called once per frame
    
     void Update()
     {
-        pivot.transform.position = target.transform.position;
-        //Get the X position of the mouse and rotate the target
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        pivot.Rotate(0, horizontal, 0);
+        transform.position=offset+target.position;
+    //     pivot.transform.position = target.transform.position;
+    //     //Get the X position of the mouse and rotate the target
+    //     float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+    //     pivot.Rotate(0, horizontal, 0);
 
-        //Get the Y position of the mouse and rotate the pivot
-        float vertical  = Input.GetAxis("Mouse Y") * rotateSpeed;
-        //pivot.Rotate(vertical,0,0);
-        if(invertY)
-        {
-            pivot.Rotate(vertical, 0, 0);
-        }
-        else
-        {
-            pivot.Rotate(-vertical, 0, 0);
-        }
+    //     //Get the Y position of the mouse and rotate the pivot
+    //     float vertical  = Input.GetAxis("Mouse Y") * rotateSpeed;
+    //     //pivot.Rotate(vertical,0,0);
+    //     if(invertY)
+    //     {
+    //         pivot.Rotate(vertical, 0, 0);
+    //     }
+    //     else
+    //     {
+    //         pivot.Rotate(-vertical, 0, 0);
+    //     }
 
-        // Limit up/down camera rotation
-        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
-        {
-            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
-        }
+    //     // Limit up/down camera rotation
+    //     if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+    //     {
+    //         pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+    //     }
 
-        if(pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
-        {
-            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
-        }
+    //     if(pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+    //     {
+    //         pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+    //     }
 
 
-        float desiredXAngle = pivot.eulerAngles.x;
-        float desiredYAngle = pivot.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
-        transform.position = target.position - (rotation * offset);
+    //     float desiredXAngle = pivot.eulerAngles.x;
+    //     float desiredYAngle = pivot.eulerAngles.y;
+    //     Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+    //     transform.position = target.position - (rotation * offset);
 
-        // tranform.position = target.position - offset;
+    //     // tranform.position = target.position - offset;
 
-        if(transform.position.y < target.position.y)
-        {
-            transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
-        }
-        transform.LookAt(target);
+    //     if(transform.position.y < target.position.y)
+    //     {
+    //         transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+    //     }
+    //     transform.LookAt(target);
     }
 
 }
