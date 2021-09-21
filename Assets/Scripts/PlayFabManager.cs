2b817cf9b2bc70e990befc72dd029e820c79a344
DeekshitaV <deekshitaverma@gmail.com>
2021-09-21 21:09:41 +0530

    LoginDatabase

---
+++ Assets/Scripts/PlayFabManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class PlayFabManager : MonoBehaviour
+{
+    public Text messageText;
+    public InputField emailInput;
+    public InputField passwordInput;
+   
+    public void LoginButton()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+        
+    }
+
+    void OnLoginSuccess(LoginResult result)
+    {
+        messageText.text = "Logged In";
+        SendLeaderboard();
+        // SendLeaderboard();
+    }
+    public void RegisterButton()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+    }
+    
+    void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        messageText.text = "Registered and logged in!";
+    }
+
+    void OnError(PlayFabError error)
+    {
+        messageText.text = error.ErrorMessage;
+    }
+
+    public void SendLeaderboard()
+    {
+        var request = new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate
+                {
+                    StatisticName = "Score",
+                    Value = 5
+                }
+            }
+        };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderboardUpdate, OnError);
+    }
+
+    void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result)
+    {
+        Debug.Log(result);
+    }
+}
