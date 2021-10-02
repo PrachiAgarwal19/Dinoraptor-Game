9ee685238f0abb89c2ce1e574dbd97f5a6203273
Manasvvi <manasvvi.aggarwal@gmail.com>
2021-10-02 21:41:36 +0530

    human movement added

--- Assets/Scripts/popupappear.cs
+++ Assets/Scripts/popupappear.cs
@@ -11,25 +11,70 @@ public class popupappear : MonoBehaviour
    private InputField InputBox;
    [SerializeField]
    private Canvas Question;
+   public static int movespeed = 3;
+    public Vector3 userDirection = Vector3.right;
 
-   void OnTriggerEnter(Collider other)
-   {
-       if (other.CompareTag("Player"))
-       {
+    public void Update()
+    {
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
            _popupapper.enabled = true;
            InputBox.ActivateInputField();
            Question.GetComponent<Canvas> ().enabled = true;
+        }
 
-       }
-   }
-   void OnTriggerExit(Collider other)
-   {
+        if(Question.GetComponent<Canvas> ().enabled == true){
+            movespeed = 0;
+        }
+        
+        if(other.CompareTag("West Wall(2)")){
+            Vector3 lookForward;
+            lookForward.x = 0f;
+            lookForward.y = 0;
+            lookForward.z = -360f;
+            transform.rotation = Quaternion.LookRotation(lookForward);
+        }
+
+        if(other.CompareTag("West Wall")){
+            Vector3 lookForward;
+            lookForward.x = -360f;
+            lookForward.y = 0;
+            lookForward.z = 0;
+            transform.rotation = Quaternion.LookRotation(lookForward);
+        }
+
+        if(other.CompareTag("West Wall(3)")){
+            Vector3 lookForward;
+            lookForward.x = 0f;
+            lookForward.y = 0;
+            lookForward.z = 360f;
+            transform.rotation = Quaternion.LookRotation(lookForward);
+        }
+
+        if(other.CompareTag("West Wall(1)")){
+            Vector3 lookForward;
+            lookForward.x = 360f;
+            lookForward.y = 0;
+            lookForward.z = 0;
+            transform.rotation = Quaternion.LookRotation(lookForward);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
        if (other.CompareTag("Player"))
        {
            _popupapper.enabled = false;
            InputBox.DeactivateInputField();
            Question.GetComponent<Canvas> ().enabled = false;
-
+           movespeed = 3;
        }
-   }
+    }
+
+    
 }
