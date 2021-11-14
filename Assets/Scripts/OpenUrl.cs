a4402b72b16ec3261f7af2f2211a2cfbe15543c8
Manasvvi <manasvvi.aggarwal@gmail.com>
2021-11-14 11:03:59 +0530

    added openURL in new tab and some minor changes

--- Assets/Scripts/OpenUrl.cs
+++ Assets/Scripts/OpenUrl.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OpenUrl : MonoBehaviour
 {
     public string url;
 
     public void Open(){
-        Application.OpenURL(url);
+        //Application.OpenURL(url,"_blank").focus();
+        //Application.ExternalEval("window.open(\"http://www.unity3d.com\")");
+        //Application.ExternalEval("window.open('https://www.hackerrank.com/room-1-infinity-code-a')");
+        Application.ExternalEval("window.open('" + url + "', '_blank')");
     }
 }
